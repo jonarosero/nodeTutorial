b95f1a34a9686f7c79ae71a2bdec9565cd1b7fc0
InDevelopment Company <indevelopmentcompany@iMac-de-utpl-9.local>
2019-09-11 17:09:49 -0500

    connect with cluster in atlas(mongo db)

--- components/message/store.js
+++ components/message/store.js
@@ -1,12 +1,23 @@
-const list = [];
+const db = require('mongoose');
+
+//mongodb+srv://test:test1234@dbcluster-aegol.mongodb.net/test?retryWrites=true&w=majority
+
+db.Promise = global.Promise;
+db.connect('mongodb+srv://test:test1234@dbcluster-aegol.mongodb.net/test?retryWrites=true&w=majority',{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).catch(e => console.log(e));
+
+
 
 
 function addMessage(message){
-    list.push(message);
+    const myMessage = new Model (message);
+    myMessage.save();
 }
 
 function getMessages(){
-    return list;
+    //return list;
 }
 
 module.exports = {
