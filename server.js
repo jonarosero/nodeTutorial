55a4e5cb0215e50c68302f47726e48481f4222c5
jonarosero <jars9605@hotmail.com>
2019-09-15 23:30:17 -0500

    update

--- server.js
+++ server.js
@@ -1,15 +1,25 @@
 const express = require('express');
+const app = express();
+const server = require('http').Server(app);
+
+const cors = require('cors');
 const bodyParser = require('body-parser');
+const socket = require('./socket');
 const db = require('./db');
 const router = require('./network/routes');
 
 db('mongodb+srv://test:test1234@dbcluster-aegol.mongodb.net/test?retryWrites=true&w=majority')
 
-var app = express();
+app.use(cors());
+
 app.use(bodyParser.json());
+
+socket.connect(server);
+
 router(app);
 
 app.use('/app', express.static('public'));
 
-app.listen(3000);
-console.log('La aplicacion esta escuchando en http://localhost:3000')
+server.listen(3000, function(){
+    console.log('La aplicacion esta escuchando en http://localhost:3000')
+});
---
+++ socket.js
@@ -0,0 +1,12 @@
+const socketIo = require('socket.io');
+const socket = {};
+
+
+function connect(server){
+    socket.io = socketIo(server);
+};
+
+module.exports ={
+    connect,
+    socket
+};
